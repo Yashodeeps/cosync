8fa4e0b5768d12589ed85a40cc1c5fedb1eab0db
Yashodeep Nimbekar <117455443+Yashodeeps@users.noreply.github.com>
2024-10-15 20:20:45 +0530

    feat: add text and sticky note elements

--- src/components/room/LayerPreview.tsx
+++ src/components/room/LayerPreview.tsx
@@ -2,8 +2,9 @@ import { Layer, LayerType, RectangleLayer } from "@/types/canvas";
 import { useStorage } from "@liveblocks/react";
 import React, { memo } from "react";
 import Rectangle from "./objects/Rectangle";
-import { Ellipsis } from "lucide-react";
 import Ellipse from "./objects/Ellipse";
+import Text from "./objects/Text";
+import StickyNote from "./objects/StickyNote";
 
 interface LayerPreviewProps {
   id: string;
@@ -14,14 +15,33 @@ interface LayerPreviewProps {
 const LayerPreview = memo(
   ({ id, onLayerPointerDown, selectionColor }: LayerPreviewProps) => {
     const layer = useStorage((root) => root.layers.get(id));
-    if (!layer) {
-      return null;
-    }
 
     const isRectangleLayer = (layer: Layer): layer is RectangleLayer =>
       layer.type === LayerType.Rectangle;
 
+    if (!layer) {
+      return null;
+    }
+
     switch (layer.type) {
+      case LayerType.Note:
+        return (
+          <StickyNote
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
       case LayerType.Ellipse:
         return (
           <Ellipse
---
+++ src/components/room/objects/Text.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Kalam } from "next/font/google";
+import { TextLayer } from "@/types/canvas";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+import { cn, colorToCss } from "@/lib/utils";
+import { useMutation } from "@liveblocks/react";
+
+const font = Kalam({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const calculateFontSize = (width: number, height: number) => {
+  const maxFontSize = 96;
+  const scaleFactor = 0.5;
+  const fontSizeBasedOnHeight = height * scaleFactor;
+  const fontSizeBasedOnWidth = width * scaleFactor;
+
+  return Math.min(maxFontSize, fontSizeBasedOnHeight, fontSizeBasedOnWidth);
+};
+
+interface TextProps {
+  id: string;
+  layer: TextLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+const Text = ({ id, layer, onPointerDown, selectionColor }: TextProps) => {
+  const { x, y, width, height, color, value } = layer;
+
+  const updateValue = useMutation(({ storage }, newValue: string) => {
+    const liveLayers = storage.get("layers");
+
+    liveLayers.get(id)?.set("value", newValue);
+  }, []);
+
+  const handleContentChange = (e: ContentEditableEvent) => {
+    updateValue(e.target.value);
+  };
+
+  return (
+    <foreignObject
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        outline: selectionColor ? `1px solid ${selectionColor}` : "none",
+      }}
+    >
+      <ContentEditable
+        html={value || "cosync"}
+        onChange={handleContentChange}
+        className={cn(
+          "h-full w-full flex items-center justify-center text-center drop-shadow-md outline-none",
+          font.className
+        )}
+        style={{
+          fontSize: calculateFontSize(width, height),
+          color: color ? colorToCss(color) : "#000",
+        }}
+      />
+    </foreignObject>
+  );
+};
+
+export default Text;
